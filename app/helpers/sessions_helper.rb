9a357ad9bf1f9762bbe9e8bb9c9c0fdd1114a7d6
atsunori.nishiyama <nishiyama1980@icloud.com>
2019-08-11 04:36:30 +0000

    user login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,26 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # def current_user
+  # if @current_user
+  #   return @current_user
+  # else
+  #   @current_user = User.find_by(id: session[:user_id])
+  #   return @current_user
+  # end
+  # end
+  
+  def logged_in?
+    !!current_user
+  end
+end
+
+  # def logged_in?
+  # if current_user
+  #   return true
+  # else
+  #   return false
+  # end
+  # end
